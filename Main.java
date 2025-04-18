671973d3c8027f40142d0784d9f042b0e61d7f0f
floromo <145050916+floromo@users.noreply.github.com>
2025-04-18 23:28:33 +0000

    donezo

--- Main.java
+++ Main.java
@@ -8,9 +8,38 @@ public class Main
     Scanner sc = new Scanner(System.in);
     String input = "";
 
+    ArrayList<String> words = new ArrayList<>();
+
+    System.out.println("Please enter words, enter STOP to stop the loop.");
     while (!input.equals("STOP"))
     {
-      System.out.println("infinite loop! replace with your code");
+      input = sc.nextLine();
+      words.add(input);
     }
+    words.remove(words.size()-1);
+    System.out.println(words.size());
+    System.out.print("[");
+    for (String word : words) {
+      System.out.print(word);
+      if (word != words.get(words.size()-1)){
+        System.out.print(", ");
+      }
+    }
+    System.out.println("]");
+
+    if (words.size() > 2){
+      words.remove(words.size()-1);
+      words.add(words.get(0));
+      words.remove(0);
+    }
+
+    System.out.print("[");
+    for (String word : words) {
+      System.out.print(word);
+      if (word != words.get(words.size()-1)){
+        System.out.print(", ");
+      }
+    }
+    System.out.println("]");
   }
 }
